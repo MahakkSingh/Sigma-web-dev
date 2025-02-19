c31357b58ac77a2408c217408bce8276cc2c2059
Mahak Singh <mahaksingh058@gmail.com>
2025-02-19 17:22:17 +0530

    updated

---
+++ JAVASCRIPT_FOLDER/intro/script.js
@@ -0,0 +1,24 @@
+alert("hello guys")
+
+console.log("hello brother")
+
+console.log("ankh lda k tune mara")
+console.log("ghayal ho gya dil bechara")
+
+// var a = prompt("enter your number: ")
+
+// console.log("your number is "+ a)
+
+var b = prompt("enter your dob")
+
+console.log ("your dob is "+ b)
+
+var isTrue= confirm("are you sure you want to leave this page")
+
+if(isTrue){
+    console.log("page closing")
+}
+
+else{
+    console.log("page aint closing duh")
+}
---
+++ JAVASCRIPT_FOLDER/variables/script.js
@@ -0,0 +1,31 @@
+console.log("hey this is intro to variables in java script")
+
+
+var a= 10;
+ var b= 15;
+ var c =20 ;
+
+console.log(a+b+c)
+
+console.log(typeof a, typeof b, typeof c)
+ 
+
+
+// const a1= 54;
+// console.log(a1)
+
+let x  = 22;
+let  y = 0.5;
+
+const p = true;
+
+let z = "mhk";
+let  q = undefined;
+let  r = null;
+
+console.log(x,y,z,p,q,r);
+
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+
+
+ 
